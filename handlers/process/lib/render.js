5c45cb7b7ec768537f09bc22f112f55934981e6d
joshua sorkin <joshua.d.sorkin@gmail.com>
2022-08-02 08:10:39 -0700

    replace apostrophe with escape apostrophe instead of escape double quote

--- handlers/process/lib/render.js
+++ handlers/process/lib/render.js
@@ -10,7 +10,7 @@ const quoteTemplate = require('../../../templates/quote.json');
 
 const cleanString = async (string) => {
   let newString = string.replace(/(\r\n|\n|\r)/gm, '<br>');
-  newString = string.replace(/'/g, '&quot;');
+  newString = string.replace(/'/g, '&apos;');
   newString = newString.replace(/"/g, '&quot;');
   return newString;
 };
